fd8a20c7818946266991cfad2ccc9a44a3fbe980
Asin-Junior-Honore <asinhonore823@gmail.com>
2024-03-05 16:11:10 +0100

    Implemented React contact page project for the React Assignment, meeting all specified requirements and deadline

--- src/app/components/FormComponet.tsx
+++ src/app/components/FormComponet.tsx
@@ -22,7 +22,9 @@ const FormComponent = () => {
     const schema = yup.object().shape({
         name: yup.string().required("Name is required"),
         email: yup.string().email("Invalid email").required("Email is required"),
-        phone: yup.string().required("Phone is required"),
+        phone: yup.string()
+            .matches(/^[+]?[0-9]+$/, 'Phone number must be numeric')
+            .required("Phone is required"),
         message: yup.string().required("Message is required"),
     });
 
